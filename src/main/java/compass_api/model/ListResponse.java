5df296269096cea26ae5063a54bf921b2c51c1d9
Emre Kaplan <emrekp@outlook.com>
2020-10-16 01:13:33 +0300

    Add generic list template for pagination type responses

--- src/main/java/compass_api/model/ListResponse.java
+++ src/main/java/compass_api/model/ListResponse.java
@@ -1,38 +1,13 @@
 package compass_api.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.ArrayList;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ListResponse<T> {
 
-  private Integer page;
-  private Integer perPage;
-  private Integer total;
   private List<T> data;
-
-  public Integer getPage() {
-    return page;
-  }
-
-  public void setPage(Integer page) {
-    this.page = page;
-  }
-
-  public Integer getPerPage() {
-    return perPage;
-  }
-
-  public void setPerPage(Integer perPage) {
-    this.perPage = perPage;
-  }
-
-  public Integer getTotal() {
-    return total;
-  }
-
-  public void setTotal(Integer total) {
-    this.total = total;
-  }
 
   public List<T> getData() {
     return data;
